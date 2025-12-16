6ac2da86b3b720f199d50fd8b9dd08de8c110ce7
varunkothareddy <varunkothareddy01@gmail.com>
2025-12-16 17:34:58 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const PHONEPE_CONFIG = {
+  MERCHANT_ID: "SU2512151240198857739090",
+  SALT_KEY: "a0c94696-58f1-4a38-b7ee-d342d17ac167",
+  SALT_INDEX: "1",
+
+  PAY_API_URL: "https://api.phonepe.com/apis/hermes/pg/v1/pay",
+
+  REDIRECT_URL: "https://attmia.com/payment-status.html",
+  CALLBACK_URL: "https://phonepe-render-backend.onrender.com/callback"
+};
