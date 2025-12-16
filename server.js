b4df75e541884c6bb9853a43818c4f013f7fc245
varunkothareddy <varunkothareddy01@gmail.com>
2025-12-16 19:02:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
-import cors from "cors";
+
 import Razorpay from "razorpay";
 import crypto from "crypto";
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
 const razorpay = new Razorpay({
@@ -60,3 +60,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
